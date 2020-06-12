58a843173f2608ccf32a351875c27e8f053d575c
Simon Dell <simon.dell@founders4schools.org.uk>
2020-06-12 17:41:10 +0100

    Add a fallback to LazyImage. Not lazy yet.

--- src/Shared/LazyImage/LazyImage.tsx
+++ src/Shared/LazyImage/LazyImage.tsx
@@ -1,18 +1,29 @@
 import React from 'react'
+import Bars from './bars-1s-200px.gif'
 
 interface Props {
-	alt: string,
-	className?: string,
-	src: string,
+  alt: string,
+  className?: string,
+  src: string,
 }
 
 export default ({
-	alt,
-	className,
-	src
-}: Props) =>
+  alt,
+  className,
+  src
+}: Props) => {
+  const [path, setPath] = React.useState(src)
+
+  function onError () {
+    setPath(Bars)
+  }
+
+  return (
     <img
       alt={alt}
       className={className}
-      src={src}
+      onError={onError}
+      src={path}
     />
+  );
+}
